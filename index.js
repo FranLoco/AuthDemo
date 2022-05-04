538294ac94a58b7069f59b2a61af444d6760e370
Fran Sporish <franvvg1@gmail.com>
2022-05-04 16:24:44 +0200

    adding requireLogin middleware

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.use(session({
     saveUninitialized: true,
 }))
 
+const requireLogin = (req,res,next) =>{
+    if(!req.session.user_id){
+        return res.redirect('/login')
+    }
+    next();
+}
+
 app.get('/', (req,res) =>{
     res.send('HOME PAGE')
 })
@@ -66,12 +73,13 @@ app.post('/logout',(req,res) =>{
     res.redirect('/login');
 })
 
-app.get('/secret',(req,res) =>{
-    if(!req.session.user_id){
-        return res.redirect('/login')
-    }
+app.get('/secret',requireLogin,(req,res) =>{
     res.render('secret')
 }) 
+
+app.get('/topsecret',requireLogin,(req,res) =>{
+    res.send('TOP SECRET!!!!')
+})
 
 app.listen(3000, () =>{
     console.log("APP IS WORKING!")
